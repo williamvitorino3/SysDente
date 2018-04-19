10aea9ff3df0cf535f9bedfa402012cd3b607234
William Vitorino <williamvitorino3@gmail.com>
2018-04-19 09:56:03 -0300

    Buils Failed no dotnet ef add migrations

--- api/Models/DataContext.cs
+++ api/Models/DataContext.cs
@@ -1,4 +1,7 @@
+using System.IO;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 namespace SysDente.Models
 {
   public class DataContext : DbContext
@@ -20,12 +23,38 @@ namespace SysDente.Models
     public DbSet<Fornecedor> Fornecedores{ get; set; }
     public DbSet<HistoricoPaciente> HistoricoPacientes{ get; set; }
     public DbSet<Paciente> Pacientes{ get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      // Obtém as configurações especificadas em appsettings.json
+      var config = new ConfigurationBuilder()
+          .SetBasePath(Directory.GetCurrentDirectory())
+          .AddJsonFile("appsettings.json")
+          .Build();
+
+      // Define a base de dados a ser usada
+      var cnn = config.GetConnectionString("ModelConnection");
+      optionsBuilder.UseSqlite(cnn);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Relacionamento Many-to-Many (chaves compostas)
+        // modelBuilder.Entity<ProdutoEtiqueta>()
+        //     .HasKey(pt => new { ProductId = pt.ProdutoId, TagId = pt.EtiquetaId });
+
+        // Chaves Únicas
+        // modelBuilder.Entity<Cliente>().HasIndex(c => c.PessoaId).IsUnique(true);
+        // modelBuilder.Entity<Empregado>().HasIndex(e => e.PessoaId).IsUnique(true);
+        // modelBuilder.Entity<Gerente>().HasIndex(m => m.PessoaId).IsUnique(true);
+
+        // Solução OnDelete
+        //modelBuilder.Entity<ProdutoEtiqueta>()
+        //    .HasOne(p => p.Produto)
+        //    .WithMany(m => m.ProdutosEtiquetas)
+        //    .OnDelete(DeleteBehavior.Restrict);
+
+    }
   }
 
-  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-      
-      optionsBuilder.UseSqlServer(options => options.UseSqlServer(Configuration.GetConnectionString("ModelConnection")));
-    }
-} 
-    
+}
